f51ab5dc82c58d68881a3c51c9651e1057927230
Robert Steven Delgado Peralta <rs19990801@gmail.com>
2024-07-29 21:42:35 -0500

    avance manejo de errores en el formulario signup

--- Tasks/views.py
+++ Tasks/views.py
@@ -5,19 +5,23 @@ from django.http import HttpResponse
 
 # Create your views here.
 
+
 def home(request):
     return render(request, 'home.html')
+
 
 def signup(request):
     if (request.method == "POST"):
         if (request.POST['password1'] == request.POST['password2']):
             try:
-                user = User.objects.create_user(username = request.POST['username'], password = request.POST('password1'))
+                user = User.objects.create_user(
+                    username=request.POST['username'], password=request.POST['password1'])
+                print(user)
                 user.save()
                 return HttpResponse("User created successfully")
             except:
-                return HttpResponse("Username already exist")
+                return render(request, 'signup.html', {'form': UserCreationForm, 'error': 'User already exists'})
         else:
-            return HttpResponse("Password don't match")
+            return render(request, 'signup.html', {'form': UserCreationForm, 'error': "Password don't match"})
     else:
         return render(request, 'signup.html', {'form': UserCreationForm})
